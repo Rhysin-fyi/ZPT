fb6f1ff58c6c9b378e83a8c19b5e74914ab02edf
Rhysin-fyi <edwinsberry98@gmail.com>
2025-07-07 17:48:29 -0400

    Refactor Loadmodules function to the actual modules.zig file

--- src/main.zig
+++ src/main.zig
@@ -1,53 +1,9 @@
 const std = @import("std");
 const modules = @import("modules.zig");
 
-pub fn LoadModules(modulename: []const u8, stdout: anytype, stdin: anytype, buf: []u8) !void {
-    const LoadMods = modules.ModulesAvail;
-    const Module = modules.Module;
-
-    var selectedModule = Module{
-        .name = "",
-        .descrption = "",
-    };
-
-    var found = false;
-    for (LoadMods) |m| {
-        if (std.mem.eql(u8, modulename, m.name)) {
-            found = true;
-            selectedModule = m;
-            try stdout.print("Loading '{s}'\n", .{selectedModule.name});
-            break;
-        }
-    }
-
-    if (!found) {
-        try stdout.print("Module name '{s}' not found \n", .{modulename});
-    }
-
-    while (found == true) {
-        try stdout.print("zpt/{s}> ", .{selectedModule.name});
-        const line = try stdin.readUntilDelimiterOrEof(buf, '\n');
-
-        if (line == null) break;
-
-        const input = std.mem.trim(u8, line.?, " \r\n");
-
-        var parts = std.mem.tokenizeSequence(u8, input, " ");
-        const splitLine = parts.next() orelse continue;
-
-        if (std.mem.eql(u8, splitLine, "exit")) {
-            try stdout.print("Exiting...\n", .{});
-            break;
-        } else if (std.mem.eql(u8, splitLine, "help")) {
-            try stdout.print("Help: {s}\n", .{selectedModule.descrption});
-        }
-    }
-}
-
 pub fn main() !void {
     const stdout = std.io.getStdOut().writer();
     const stdin = std.io.getStdIn().reader();
-
     var buf: [1024]u8 = undefined;
 
     while (true) {
@@ -72,7 +28,7 @@ pub fn main() !void {
             try stdout.print("    load <module>\n\n", .{});
         } else if (std.mem.eql(u8, cmd, "load")) {
             const moduleName = parts.next() orelse continue;
-            try LoadModules(moduleName, stdout, stdin, buf[0..]);
+            try modules.LoadModules(moduleName, stdout, stdin, buf[0..]);
         } else if (std.mem.eql(u8, cmd, "list")) {
             const ListedMods = modules.GetAllModules();
 
--- src/modules.zig
+++ src/modules.zig
@@ -14,3 +14,45 @@ pub const ModulesAvail = [_]Module{
 pub fn GetAllModules() []const Module {
     return ModulesAvail[0..];
 }
+
+pub fn LoadModules(modulename: []const u8, stdout: anytype, stdin: anytype, buf: []u8) !void {
+    const LoadMods = ModulesAvail;
+
+    var selectedModule = Module{
+        .name = "",
+        .descrption = "",
+    };
+
+    var found = false;
+    for (LoadMods) |m| {
+        if (std.mem.eql(u8, modulename, m.name)) {
+            found = true;
+            selectedModule = m;
+            try stdout.print("Loading '{s}'\n", .{selectedModule.name});
+            break;
+        }
+    }
+
+    if (!found) {
+        try stdout.print("Module name '{s}' not found \n", .{modulename});
+    }
+
+    while (found == true) {
+        try stdout.print("zpt/{s}> ", .{selectedModule.name});
+        const line = try stdin.readUntilDelimiterOrEof(buf, '\n');
+
+        if (line == null) break;
+
+        const input = std.mem.trim(u8, line.?, " \r\n");
+
+        var parts = std.mem.tokenizeSequence(u8, input, " ");
+        const splitLine = parts.next() orelse continue;
+
+        if (std.mem.eql(u8, splitLine, "exit")) {
+            try stdout.print("Exiting...\n", .{});
+            break;
+        } else if (std.mem.eql(u8, splitLine, "help")) {
+            try stdout.print("Help: {s}\n", .{selectedModule.descrption});
+        }
+    }
+}
